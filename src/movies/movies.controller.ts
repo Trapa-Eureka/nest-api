74c18799c3c185ce947f926a750fcc93553b069c
JINHO SON <a1234@1234s-MacBook-Pro.local>
2021-01-04 23:33:28 +0900

    add Routes

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Controller, Get, Param, Post, Delete, Patch, Body, Query } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -7,23 +7,32 @@ export class MoviesController {
         return 'This will return all movies';
     }
 
-    @Get('/:id')
+    @Get("search")
+    search(@Query('year') searchingYear: string) {
+        return `We are searching for a movie made after: ${searchingYear}`;
+    }
+
+    @Get(':id')
     getOne(@Param('id') movieId: String) {  // id라는 Param을 id라는 args에 저장.
         return `This will return one movie with the id: ${movieId}`;
     }
 
     @Post()
-    create() {
-        return 'This will create a movie';
+    create(@Body() movieData) {
+        return movieData;
     }
 
-    @Delete('/:id')
+    @Delete(':id')
     remove(@Param('id') movieId: String) {
         return `This will delete a movie with the id: ${movieId}`;
     }
 
-    @Patch('/:id')
-    patch(@Param('id') movieId: string) {
-        return `This will patch a movie with the id: ${movieId}`;
+    @Patch(':id')
+    patch(@Param('id') movieId: string, @Body() updateData) {
+        return {
+            // 업데이트할 movie의 id와 보낼 데이터의 obj를 리턴
+            updatedMovie: movieId,
+            ...updateData,
+        };
     }
 }
